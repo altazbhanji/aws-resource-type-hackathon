dc9616d9ca2522a9fba2219b24c3aa9b83923988
Altaz Bhanji <abhanji@enquizit.com>
2021-05-25 14:05:30 -0400

    update all handlers to pass cfn test

--- src/eq_monitor_nagios/handlers.py
+++ src/eq_monitor_nagios/handlers.py
@@ -40,6 +40,7 @@ default_server_name = 'Nagios Server'
 default_instance_type = 't2.small'
 default_ssm_ami_parameter = '/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2'
 default_callback_period = 60
+default_ssm_path = '/Eq/Nagios/Monitor/Stack'
 
 
 # =====================================
@@ -77,11 +78,13 @@ def build_instance(model:ResourceModel, session):
 
         # get success progress event
         progress = ProgressEvent(status=OperationStatus.IN_PROGRESS, callbackDelaySeconds=default_callback_period, resourceModel=model)
+        LOG.info(f"Returning IN_PROGRESS")
 
     except Exception as err:
         # get failed progress event
         msg = f"Unexpected error creating nagios server: {type(err).__name__}: {str(err)}"
         progress = ProgressEvent(status=OperationStatus.FAILED, resourceModel=model, message=msg)
+        LOG.exception(msg)
 
     return progress
 
@@ -105,21 +108,25 @@ def check_instance_state(model:ResourceModel, session):
             # we're done
             LOG.info(f"Instance is {state_name}")
             progress = ProgressEvent(status=OperationStatus.SUCCESS, resourceModel=model)
+            LOG.info(f"Returning SUCCESS")
         elif state_code == 0:
             # still in pending state
             LOG.info(f"Instance is {state_name}")
             msg = "Waiting for EC2 instance to stabilize"
             progress = ProgressEvent(status=OperationStatus.IN_PROGRESS, resourceModel=model, message=msg)
+            LOG.info(f"Returning IN_PROGRESS")
         else:
             # something went wrong - EC2 is stopping, stopped or terminated
             LOG.info(f"Instance is {state_name}")
             msg = f"Unexpected error, instance is {state_name}"
             progress = ProgressEvent(status=OperationStatus.FAILED, resourceModel=model, message=msg)
+            LOG.info(f"Returning IN_FAILED")
 
     except Exception as err:
         # get failed progress event
         msg = f"Unexpected error checking state: {type(err).__name__}: {str(err)}"
         progress = ProgressEvent(status=OperationStatus.FAILED, resourceModel=model, message=msg)
+        LOG.exception(msg)
 
     return progress
 
@@ -140,26 +147,61 @@ def create_handler(
     #   callback, waiting for instance to spin up
     #   handle case when subnet / security group is provided
     #   process userdata for nagios
+    #   check if default subnet / sg exists if not provided
+    #   add tags
+
+    print("Starting create_handler")
 
     # initialize model
     model = request.desiredResourceState
 
+    print(f"Model is {model}")
+
     # check whether this is the first call or a callback
     # first call, create instance, subsequently check instance state until ready
-    if model.Id is None or model.Id == '':
+    if model.Id is None:
+        LOG.info(f"model.Id is {model.Id}, creating instance")
         progress = build_instance(model, session)
     else:
+        LOG.info(f"model.Id is {model.Id}, checking instance state")
         progress = check_instance_state(model, session)
+        if progress.status == OperationStatus.SUCCESS:
+            # Store server id information in SSM
+            ssm_client = session.client('ssm')
+            ssm_name = f"{default_ssm_path}/{request.stackId}"
+            ssm_value = model.Id
+            ssm_client.put_parameter(Name=ssm_name, Value=ssm_value, Type='String')
 
     return progress
 
 
 @resource.handler(Action.UPDATE)
-def update_handler(_s, request: ResourceHandlerRequest, _c) -> ProgressEvent:
-    model = request.desiredResourceState
-    progress: ProgressEvent = ProgressEvent(
-        status=OperationStatus.SUCCESS, resourceModel=model,
-    )
+def update_handler(
+    session: Optional[SessionProxy],
+    request: ResourceHandlerRequest,
+    callback_context: MutableMapping[str, Any],
+) -> ProgressEvent:
+
+    LOG.info("Starting update_handler")
+
+    # TODO:
+    #   re-launch in a new subnet if required
+    #   check tags
+    desired_state = request.desiredResourceState
+    current_state = request.previousResourceState
+
+    desired_name = desired_state.Name
+    current_name = current_state.Name
+
+    if desired_name != current_name:
+        instance_id = desired_state.Id
+        ec2_client = session.client('ec2')
+        instance = ec2_client.Instance(instance_id)
+        instance.create_tags(Tags=[{'Key': 'Name', 'Value': current_name}])
+
+
+    progress = ProgressEvent(status=OperationStatus.SUCCESS, resourceModel=desired_state)
+
     return progress
 
 
@@ -170,23 +212,49 @@ def delete_handler(
     callback_context: MutableMapping[str, Any],
 ) -> ProgressEvent:
 
-    model = request.desiredResourceState
-    instance_id = model.Id
-    if instance_id is not None:
+    LOG.info("Starting delete_handler")
+
+    ssm_client = session.client('ssm')
+    ssm_name = f"{default_ssm_path}/{request.stackId}"
+    try:
+        ssm_client.delete_parameter(Name=ssm_name)
+        model = request.desiredResourceState
+        instance_id = model.Id
         ec2_client = session.client('ec2')
         ec2_client.terminate_instances(InstanceIds=[instance_id])
-    progress: ProgressEvent = ProgressEvent(
-        status=OperationStatus.SUCCESS, resourceModel=model,
-    )
+        progress = ProgressEvent(status=OperationStatus.SUCCESS)
+        LOG.info(f"Returning SUCCESS")
+    except:
+        progress = ProgressEvent(status=OperationStatus.FAILED, errorCode=HandlerErrorCode.NotFound)
+        LOG.info(f"Returning FAILED")
+
+
     return progress
 
 
 @resource.handler(Action.READ)
-def read_handler(_s, request: ResourceHandlerRequest, _c) -> ProgressEvent:
-    model = request.desiredResourceState
-    progress: ProgressEvent = ProgressEvent(
-        status=OperationStatus.SUCCESS, resourceModel=model,
-    )
+def read_handler(
+    session: Optional[SessionProxy],
+    request: ResourceHandlerRequest,
+    callback_context: MutableMapping[str, Any],
+) -> ProgressEvent:
+    
+    LOG.info("Starting read_handler")
+
+    ssm_client = session.client('ssm')
+    ssm_name = f"{default_ssm_path}/{request.stackId}"
+    try:
+        ssm_client.get_parameter(Name=ssm_name)
+        model = request.desiredResourceState
+        instance_id = model.Id
+        ec2_client = session.client('ec2')
+        ec2_client.terminate_instances(InstanceIds=[instance_id])
+        progress = ProgressEvent(status=OperationStatus.SUCCESS, resourceModel=model)
+        LOG.info(f"Returning SUCCESS")
+    except:
+        progress = ProgressEvent(status=OperationStatus.FAILED, errorCode=HandlerErrorCode.NotFound)
+        LOG.info(f"Returning FAILED")
+    
     return progress
 
 
